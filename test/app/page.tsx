f74078ff54caf7433d30d10f67ede5017ee7b78b
Nakajima-kid <135191773+Nakajima-kid@users.noreply.github.com>
2025-09-09 15:00:21 +0700

    1

--- test/app/page.tsx
+++ test/app/page.tsx
@@ -1,6 +1,11 @@
+import { Button } from "@/components/ui/button"
+
 const HomePage = () => {
   return (
+    <>
     <div>HomePage</div>
+    <Button variant='destructive'>Submit</Button>
+    </>
   )
 }
 export default HomePage
---
+++ test/components/navbar/Navbar.tsx
@@ -0,0 +1,10 @@
+const Navbar = () => {
+  return (
+    <nav>
+      <div>
+        logo
+      </div>
+    </nav>
+  );
+};
+export default Navbar;
