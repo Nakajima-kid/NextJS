79e3679e323991e8268c226414ec6e5edcf572d5
Nakajima-kid <135191773+Nakajima-kid@users.noreply.github.com>
2025-09-09 10:58:03 +0700

    update

--- test/app/api/info/route.ts
+++
@@ -1,6 +0,0 @@
-import { log } from "console";
-
-export const GET = async () => {
-  log({ msg: "Hello" });
-  return Response.json({ msg: "Hello" });
-};
--- test/app/middleware/middleware.ts
+++
@@ -1,7 +0,0 @@
-import { log } from "console"
-import { NextResponse } from "next/server"
-
-export const middleware = (req:Request) => {
-    log('Middleware')
-    return NextResponse.redirect(new URL("/", req.url))
-}
