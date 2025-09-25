38a00a5235c47cb05ba80c5088a9189e2ae86843
Nakajima-kid <135191773+Nakajima-kid@users.noreply.github.com>
2025-09-25 10:45:06 +0700

    ๅ

--- test/components/navbar/DropdownList.tsx
+++ test/components/navbar/DropdownList.tsx
@@ -1,11 +1,34 @@
 import { AlignLeft } from "lucide-react";
-import { CircleUserRound } from "lucide-react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Button } from "@/components/ui/button";
+import UserIcon from "./UserIcon";
 
 const DropdownList = () => {
   return (
     <>
-      <AlignLeft />
-      <CircleUserRound />
+      <DropdownMenu>
+        <DropdownMenuTrigger>
+          <Button variant={'outline'}>
+            <UserIcon/>
+            <AlignLeft />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuLabel>My Account</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>Profile</DropdownMenuItem>
+          <DropdownMenuItem>Billing</DropdownMenuItem>
+          <DropdownMenuItem>Team</DropdownMenuItem>
+          <DropdownMenuItem>Subscription</DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
     </>
   );
 };
